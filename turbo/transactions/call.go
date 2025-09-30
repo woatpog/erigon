ddf1eaf9e771ff290591111aed85a554e7ef425f
lupin012 <58134934+lupin012@users.noreply.github.com.>
2025-09-30 15:28:27 +0200

    estimateGas WithBlockOverrides

--- turbo/transactions/call.go
+++ turbo/transactions/call.go
@@ -299,6 +299,7 @@ func NewReusableCaller(
 	engine consensus.EngineReader,
 	stateReader state.StateReader,
 	overrides *ethapi2.StateOverrides,
+	blockOverrides *ethapi2.BlockOverrides,
 	header *types.Header,
 	initialArgs ethapi2.CallArgs,
 	gasCap uint64,
@@ -331,6 +332,9 @@ func NewReusableCaller(
 	}
 
 	blockCtx := NewEVMBlockContext(engine, header, blockNrOrHash.RequireCanonical, tx, headerReader, chainConfig)
+	if blockOverrides != nil {
+		blockOverrides.Override(&blockCtx)
+	}
 	txCtx := core.NewEVMTxContext(msg)
 
 	evm := vm.NewEVM(blockCtx, txCtx, ibs, chainConfig, vm.Config{NoBaseFee: true})
